9e6ef105d811fbcc2a817ec20074eba1223c956f
Stephen <hellostephenwm@gmail.com>
2025-01-09 17:39:08 -0500

    7.7 => feat: render notes in material table

--- react-materialui/src/components/Notes.jsx
+++ react-materialui/src/components/Notes.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+	Table,
+	TableContainer,
+	TableBody,
+	TableCell,
+	TableRow,
+	Paper,
+} from "@mui/material";
 
 const Notes = ({ notes }) => {
 	return (
 		<div>
 			<h2>Notes</h2>
-			<ul>
-				{notes.map((note) => (
-					<li key={note.id}>
-						<Link to={`/notes/${note.id}`}>{note.content}</Link>
-					</li>
-				))}
-			</ul>
+			<TableContainer component={Paper}>
+				<Table>
+					<TableBody>
+						{notes.map((note) => (
+							<TableRow key={note.id}>
+								<TableCell>
+									<Link to={`/notes/${note.id}`}>{note.content}</Link>
+								</TableCell>
+								<TableCell>{note.user}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
 		</div>
 	);
 };
